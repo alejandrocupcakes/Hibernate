d5002ba5338c2d9e431236fa27ebdee724106c34
alejandrocupcakes <148361406+alejandrocupcakes@users.noreply.github.com>
2024-11-05 07:47:55 -0500

    Add files via upload

---
+++ JPAUtil.java
@@ -0,0 +1,20 @@
+package com.mycompany.hibernation2;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JPAUtil {
+    private static final String UNIDAD_DE_PERSISTENCIA="PruebaHibernate";
+private static EntityManagerFactory factory;
+public static EntityManagerFactory getEntityManagerFactory (){
+if (factory==null) {
+factory=Persistence.createEntityManagerFactory(UNIDAD_DE_PERSISTENCIA);
+}
+return factory;
+}
+public static void shutdown (){
+if (factory!=null) {
+factory.close();
+}
+}
+}
